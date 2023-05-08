d588a7c342556e9d8a5b20833c6d4cbf2e6c433f
Ryder_Aguilera <ryderaguilera@hotmail.com>
2023-05-08 09:16:44 -0700

    Error handeling

--- app.js
+++ app.js
@@ -38,19 +38,22 @@ app.use(cookieParser());
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
 });
 
 module.exports = app;
